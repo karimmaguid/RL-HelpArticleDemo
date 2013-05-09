947490ef0ce77d026a3b0b8eeb17d4c273cab71f
Karim Maguid <kmaguid@rocketlawyer.com>
2013-05-08 17:20:32 -0700

    new folders

--- js/app.js
+++ js/app.js
@@ -7,14 +7,6 @@ config(['$routeProvider',
             templateUrl: 'templates/home.html',
             controller: CategoryListCtrl
         }).
-        when('/category/:categoryText', {
-            templateUrl: 'templates/category.html',
-            controller: CategoryDetailCtrl
-        }).
-        when('/article/:articleText', {
-            templateUrl: 'templates/article.html',
-            controller: ArticleDetailCtrl
-        }). 
         otherwise({
             redirectTo: '/home'
         });
--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,13 @@
 /* Controllers */
 
-function CategoryListCtrl($scope, $location, Categories) {
-	//pass categories into controllers scope
-    $scope.categories = Categories.query();
+function CategoryListCtrl($scope, Categories) {
+	$scope.categories = Categories.query();
 }
 
 function CategoryDetailCtrl($scope, $routeParams, Category) {
-    var catText = $routeParams.categoryText.toLowerCase();
-    $scope.category = Category.get({
-       categoryText: catText
-    }, function (category) {
-    });
- 
+
 }
 
 function ArticleDetailCtrl($scope, $routeParams, Article) {
-    var artText = $routeParams.articleText.toLowerCase();
-    $scope.article = Article.get({
-       articleText: artText
-    }, function (article) {
-    	console.log(article);
-    });
+
 }
